93d43d4534f7b2a0d2d8c6a63d631852e4e637c4
deependra04 <kdeep05.dg@gmail.com>
2025-08-28 12:02:52 +0530

    added cors

--- backend/app/config.py
+++ backend/app/config.py
@@ -36,6 +36,8 @@ class Settings(BaseSettings):
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:8080",
+        "https://fastapi-blog-website-2.onrender.com",
+        "https://fastapi-blog-website-2.onrender.com/",
     ]
 
     # API settings
